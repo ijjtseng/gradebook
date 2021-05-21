9d0587d99a39cfc29a1b2809b948e0ddcad5635e
Joyce Tseng <ijjtseng@Joyces-MacBook-Pro.local>
2021-05-22 04:59:30 +0900

    Lists, namespace, format floating point number

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,23 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            var numbers = new[] {10.2, 33.4, 25.2222, 11.1};
-            var result = 0.0; 
-            foreach(var number in numbers)
-            {
-                result += number;
-            }
+        {            
+            var grades = new List<double>() {10.2, 33.4, 25.2222, 11.1};
+            grades.Add(22.2);
+            var numofGrades = grades.Count;
 
-            Console.WriteLine(result);
+            var sum = 0.0; 
+            foreach(var number in grades)
+            {
+                sum += number;
+            }
+            
+            var avg = sum / numofGrades;
+
+            Console.WriteLine($"Total: {sum:N3}, Avg: {avg:N1}");
 
             if(args.Length > 0)
             {
-                Console.WriteLine($"Total Score:");
+                Console.WriteLine($"Total Score: {sum}");
             }
             else
             {
