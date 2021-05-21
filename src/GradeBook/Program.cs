6c1c828f3b1b797368391d420e15ce0f75181016
Joyce Tseng <ijjtseng@Joyces-MacBook-Pro.local>
2021-05-22 04:38:00 +0900

    looping thru array

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,12 +6,18 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            double x = 333.33;
-            double y = 11.1 + x;
+            var numbers = new[] {10.2, 33.4, 25.2222, 11.1};
+            var result = 0.0; 
+            foreach(var number in numbers)
+            {
+                result += number;
+            }
+
+            Console.WriteLine(result);
 
             if(args.Length > 0)
             {
-                Console.WriteLine($"Total Score: {y}");
+                Console.WriteLine($"Total Score:");
             }
             else
             {
