0917775b85e4cc7400a45ee7d9828a39ef371418
Joyce Tseng <ijjtseng@Joyces-MacBook-Pro.local>
2021-06-01 03:03:24 +0900

    passing parameters by value or ref

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,54 @@ namespace GradeBook.Tests //.Tests implies being inside GradeBook namespace, the
     public class TypeTests
     {
         [Fact]
+        public void CSharpCouldPassByRef() // ref!!
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetNameByRef(ref book1, "New Name"); // ref!!
+            
+            Assert.Equal("New Name", book1.Name);
+
+        }
+
+        private void GetBookSetNameByRef(ref Book book, string name) // ref!!
+        {
+            book = new Book(name); 
+        }
+        
+        [Fact]
+        public void CSharpIsPassByValue() //Trying to pass by reference here and failed. when passing var to method, method won't change value of original var. 
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+            
+            Assert.Equal("Book 1", book1.Name);
+
+        }
+
+        private void GetBookSetName(Book book, string name) //book value memory location is diff than var book1
+        {
+            book = new Book(name); //constructs new book obj
+            book.Name = name;
+        }
+        
+        [Fact]
+        public void CanSetNameFromReference() //Csharp is pass by value!
+        {
+            var book1 = GetBook("Book 1");
+            //var book2 = book1;
+            SetName(book1, "New Name");
+            
+            Assert.Equal("New Name", book1.Name);
+            //Assert.Equal("New Name", book2.Name);
+
+        }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name; 
+        }
+
+        [Fact]
         public void GetBookReturnsDiffObjects()
         {
             var book1 = GetBook("Book 1");
