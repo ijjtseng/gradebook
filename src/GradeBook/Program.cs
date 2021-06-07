e33d638dabb7bee62f27e12d852dbfc52e794924
Joyce Tseng <ijjtseng@Joyces-MacBook-Pro.local>
2021-06-07 23:12:06 +0900

    set up scenario for polymorphism

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,12 +7,27 @@ namespace GradeBook //keep your classes in their own namespace, avoid conflicts
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
             var book = new Book("My Gradebook");
             //do while loop to ask for book name?
             book.GradeAdded += OnGradeAdded; // things listening/subscribed to event, can only use += or -=
-             
-            while(true)
+
+            EnterGrades(book);
+
+            var stats = book.GetStats();
+            book.Name = "";
+
+            Console.WriteLine($"For the book named {book.Name}:");
+            Console.WriteLine($"Avg: {stats.Average:N1} | High Grade: {stats.High} | Low Grade: {stats.Low}");
+            Console.WriteLine($"The average letter grade is: {stats.Letter}");
+            //refactoring, unit test forces improvement in code
+
+
+        }
+
+        private static void EnterGrades(Book book) //lightbulb to extract method
+        {
+            while (true)
             {
                 Console.WriteLine("Enter grade or 'q' to quit.");
                 var input = Console.ReadLine();
@@ -27,23 +42,15 @@ namespace GradeBook //keep your classes in their own namespace, avoid conflicts
                     book.AddGrade(grade);
 
                 }
-                catch(Exception ex){
+                catch (Exception ex)
+                {
                     Console.WriteLine(ex.Message); //catching exception
                 }
-                
+
 
             };
-
-            var stats = book.GetStats();
-            book.Name = "";
-
-            Console.WriteLine($"For the book named {book.Name}:");
-            Console.WriteLine($"Avg: {stats.Average:N1} | High Grade: {stats.High} | Low Grade: {stats.Low}");
-            Console.WriteLine($"The average letter grade is: {stats.Letter}");
-            //refactoring, unit test forces improvement in code
-            
-            
         }
+
         static void OnGradeAdded(object sender, EventArgs e) //reacting to event
         {
             Console.WriteLine("A grade was added.");
